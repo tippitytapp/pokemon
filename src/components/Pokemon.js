2e76d3d158a268214fab423e0694d592ff66741f
Marc Tapp <marc.tapp@gmail.com>
2020-07-03 09:53:21 -0400

    reset search input to empty string onSubmit

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -19,7 +19,7 @@ function Pokemon(props){
         <>
                     <header className="header">
                         <h1> Pokedex</h1>
-                        <form onSubmit={(e)=>{e.preventDefault();toggle();props.fetchSearchedPokemon(` https://pokeapi.co/api/v2/pokemon/${search}`)}}>
+                        <form onSubmit={(e)=>{e.preventDefault();toggle();props.fetchSearchedPokemon(` https://pokeapi.co/api/v2/pokemon/${search}`);setSearch("")}}>
                             <label htmlFor="search">Search: </label>
                             <input
                             type="text"
